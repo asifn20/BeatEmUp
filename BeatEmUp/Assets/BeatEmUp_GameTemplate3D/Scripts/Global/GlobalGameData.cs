8b11cc6861f70779ee0d384ddb95f81c082e4a30
asifn20 <42694277+asifn20@users.noreply.github.com>
2019-08-26 16:23:22 +0530

    add commit

--- BeatEmUp/Assets/BeatEmUp_GameTemplate3D/Scripts/Global/GlobalGameData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-
-public static class GlobalPlayerData  {
-	public static GameObject Player1Prefab;
-	public static Sprite PlayerHUDPortrait;
-}
